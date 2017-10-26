01d6e6240f738dab5e989ea985418a15220ae9f4
Mohanad Aly <capmohanad@hotmail.com>
2017-10-26 11:28:25 +0300

    Change few lines

--- file.py
+++ file.py
@@ -35,17 +35,18 @@ else:
     config = open(filename, 'w')
     print (config)
 
-#if input("Do you want to create ssh key (y). Skip with Enter: "):
-#    os.system("ssh-keygen -t rsa")
+if input("Do you want to create ssh key [Y/n]. You can skip with Enter: ") == 'Y':
+    os.system("ssh-keygen -t rsa")
 print ("\n")
 
-#scan1 = input("Scan Your network for open ssh:")
-#os.system("nmap -v " + str(scan1) + "/24" + "| grep " + " 'ssh'")
+scan1 = input("Enter your IP to scan your network for open ssh: ")
+os.system("nmap -v " + str(scan1) + "/24" + "| grep " + " 'ssh'")
 print ("\n")
 
 #scan2 = input("Scan Your network for IP Addresses: ")
+#This only for testing 
 scan2 = "192.168.56.1"
-#os.system("nmap -v " + scan2 + "/24" + "| grep" + " 'port 22'")
+os.system("nmap -v " + scan2 + "/24" + "| grep" + " 'port 22'")
 
 
 #process = subprocess.Popen("nmap -v  "+ scan2 +"/24  |  grep  'port 22'",
@@ -70,23 +71,22 @@ command = stdout_list.split('\n')
 
 selected_ip = None
 for ip_string in command:
-  if ip_string:
+    if ip_string:
     #print('ass', ip_string)
-    ip = ip_string.split()[-1]
+      ip = ip_string.split()[-1]
     #print(ip_string, ip)  
-    if input(ip + "Do you want to select this IP Address [Y/n]:") == 'y':
-      selected_ip = ip
-  
-print(selected_ip, ip)  
+      if input(ip + "Do you want to select this IP Address [Y/n]:") == 'y':
+        selected_ip = ip
+print(selected_ip, ip) 
 #command = ['nmap', '-v', scan2 + '/24', '|' , 'grep'+ " " + "'port 22'"]
 #result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
 #print (result.stdout.decode('utf-8'))
 #print (command)
 #print(' '.join(command))
 print ("\n")
-
 try:
     while True:
+            ip_string
             first = input("Please Enter Machine Name: ")
             zero = input("This for the root user:")
             #second = input("Please Enter The Hostname Of the Machine Or IP Address: ")
@@ -115,15 +115,15 @@ try:
                 config.write("Host " + zero + "\n" + "Hostname " + ip + "\n" + third + "\n" + "User " + "root" + "\n")
                 config.write("\n")   
             if fifth:
-                    os.system("ssh-copy-id " + fourth + "@" + str(second))
+                    os.system("ssh-copy-id " + fourth + "@" + ip)
             if sixth:
-                    os.system("ssh -t " + fourth + "@" + str(second)  + " " + " " " 'sudo cp --parents .ssh/authorized_keys /root/' ")    
+                    os.system("ssh -t " + fourth + "@" + ip  + " " + " " " 'sudo cp --parents .ssh/authorized_keys /root/' ")    
             print ("Finished and starting with the new machine")
             print ("\n")
-            if first != fourth:
+            if first == fourth:
                 config.write("\n")
                 config.write("\n")
-except KeyboardInterrupt:    
-            config.close()
+except KeyboardInterrupt:
+  config.close()
 atexit.register(exit_handler)
 
